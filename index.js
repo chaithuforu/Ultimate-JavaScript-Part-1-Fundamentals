0a9b606fc4f4a39d51d587cae33a3de89a745a0c
chaithanya <chaithanya.m@axelerant.com>
2024-10-21 17:08:55 +0530

    LED-800: Max of 2 numbers.

---
+++ index.js
@@ -0,0 +1,9 @@
+// Function to get the maximum of two numbers.
+function findMaxNumber(a, b) {
+    return (a > b) ? a : b;
+}
+let number = findMaxNumber(10, 2);
+// Log the max number.
+console.log(number);
+console.log(findMaxNumber(5, 2));
+console.log(findMaxNumber(90, 100));
