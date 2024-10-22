cd56b2aa46585430abba9eb251bba2f5e56a56cd
chaithanya <chaithanya.m@axelerant.com>
2024-10-22 08:23:39 +0530

    LDT-800: constructor functions

---
+++ index.js
@@ -0,0 +1,10 @@
+function Blog(title, body, author) {
+    this.title = title,
+    this.body = body,
+    this.author = author,
+    this.views = 0,
+    this.comments = [],
+    this.isLive = false
+}
+const blog = new Blog("Tit;e of Blog","Blog body","Author");
+console.log(blog);
