087f49f1354ff1c57bef315cc896c1d705a0284b
chaithanya <chaithanya.m@axelerant.com>
2024-10-22 08:47:16 +0530

    LDT-800: circle area

---
+++ index.js
@@ -0,0 +1,9 @@
+let circle = {
+    radius: 2,
+    area: 0,
+    get area() {
+        return Math.PI * this.radius * this.radius;
+    }
+};
+console.log(circle.radius);
+console.log(circle.area);
