130274d685ffe9a423b733b840c3ba48e2495d0a
chaithanya <chaithanya.m@axelerant.com>
2024-10-22 08:21:53 +0530

    LDT-800: Blog post object

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const blog = {
+    title: "Title of blog",
+    body: "body  of blog",
+    author: "Author of blog",
+    views: 15,
+    comments: [
+        { author: 'Sam', body: 'b' },
+        { author: 'Tom', body: 'd' }
+    ],
+    isLive: true
+};
+console.log(blog);
